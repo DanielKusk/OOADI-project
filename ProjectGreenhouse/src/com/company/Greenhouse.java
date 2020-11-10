3365a6658f646458509b2b20e38b5baf2a18228c
RonniCarlsen <ronni_C@live.dk>
2020-11-10 09:22:42 +0100

    Oprettelse af projekt

---
+++ ProjectGreenhouse/src/com/company/Greenhouse.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+
+public class Greenhouse {
+    private LocalDateTime dateTime;
+    private double humidity;
+    private double temperature;
+    private boolean sunlight;
+    private int vacanSpots;
+    public void growPlants() {
+        if ((sunlight = true) & (temperature > 10)){
+        setGrowthProgress}
+    }
+}
+
+    waterOn()
+    WaterOff()
+    openWindow()
+    closeWindow()
+
+
+public Greenhouse(LocalDateTime dateTime, double humidity, double temperature, boolean sunlight, int vacanSpots) {
+        this.dateTime = dateTime;
+        this.humidity = humidity;
+        this.temperature = temperature;
+        this.sunlight = sunlight;
+        this.vacanSpots = vacanSpots;
+        }
