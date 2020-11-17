590881912d0f95a3f88d2e44a60f5ada9ec10e7d
christiannielsen98 <cfpn19@student.aau.dk>
2020-11-17 13:24:45 +0100

    Revert "SERVERSOCKET VIRKER"
    This reverts commit 49c6002ced165744d267300c31c8633167f07817.

--- ProjectGreenhouse/src/GreenhouseServer.java
+++ ProjectGreenhouse/src/GreenhouseServer.java
@@ -1,28 +1,25 @@
-import java.net.*;
-import java.io.*;
-
-public class GreenhouseServer {
-    public static void main(String[] args) throws IOException {
-        try {
-
-            System.out.println("Waiting for clients...");
-            ServerSocket serverSocket = new ServerSocket(9806);
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Connection established");
-            while (true) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String str = in.readLine();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                out.println("Server says:" + str);
-                if (str.equals("close")) {
-                    serverSocket.close();
-                }
-            }
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-}
+//import java.net.*;
+//import java.io.*;
+//
+//public class GreenhouseServer {
+//    private
+//    public static void main(String[] args) throws IOException {
+//        try {
+//
+//            System.out.println("Waiting for clients...");
+//            ServerSocket serverSocket = new ServerSocket(9806);
+//            Socket clientSocket = serverSocket.accept();
+//            System.out.println("Connection established");
+//            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//            String str = in.readLine();
+//            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+//            out.println("Server says:"+str);
+//
+//        }
+//        catch (IOException e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//}
--- ProjectGreenhouse/src/com/company/Greenhouse.java
+++ ProjectGreenhouse/src/com/company/Greenhouse.java
@@ -1,39 +1,39 @@
-package com.company;
-import java.time.LocalDateTime; // Import the LocalDateTime class
-import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
-import java.util.Dictionary;
-
-public class Greenhouse {
-    private LocalDateTime dateTime;
-    private double humidity;
-    private int temperature;
-    private boolean sunlight;
-    private int vacanSpots;
-
-    public Greenhouse(LocalDateTime dateTime, double humidity, int temperature, boolean sunlight, int vacanSpots) {
-        this.dateTime = dateTime;
-        this.temperature = temperature;
-        this.sunlight = sunlight;
-//        this.vacanSpots = vacanSpots;
-//        this.humidity = humidity;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-    public int getTemperature() {
-        return temperature;
-    }
-    public boolean getSunlight() {
-        return sunlight;
-    }
-
-//    public waterPlants() {
+//package com.company;
+//import java.time.LocalDateTime; // Import the LocalDateTime class
+//import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+//import java.util.Dictionary;
+//
+//public class Greenhouse {
+//    private LocalDateTime dateTime;
+//    private double humidity;
+//    private int temperature;
+//    private boolean sunlight;
+//    private int vacanSpots;
+//
+//    public Greenhouse(LocalDateTime dateTime, double humidity, int temperature, boolean sunlight, int vacanSpots) {
+//        this.dateTime = dateTime;
+//        this.temperature = temperature;
+//        this.sunlight = sunlight;
+////        this.vacanSpots = vacanSpots;
+////        this.humidity = humidity;
 //    }
 //
-//    WaterOff()
+//    public LocalDateTime getDateTime() {
+//        return dateTime;
+//    }
+//    public int getTemperature() {
+//        return temperature;
+//    }
+//    public boolean getSunlight() {
+//        return sunlight;
+//    }
 //
-//    openWindow()
-//
-//    closeWindow()
-}
+////    public waterPlants() {
+////    }
+////
+////    WaterOff()
+////
+////    openWindow()
+////
+////    closeWindow()
+//}
